d093964cdf9f956635cb48d6edd36b4b5ade9fbd
Edmond GAKS <edmondgaks@gmail.com>
2022-11-12 13:39:52 +0200

    adding routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
-const port = 5000
+const port = process.env.PORT || 5000
+const goalRoutes = require('./routes/goalRoutes');
 
 const app = express();
+
+app.use('/api/goals', goalRoutes);
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
