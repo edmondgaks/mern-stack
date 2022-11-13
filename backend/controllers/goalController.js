c5c97e2bab69b2c31b051f82a75265ba705893ff
Edmond GAKS <edmondgaks@gmail.com>
2022-11-13 11:37:50 +0200

    getting goals by also the user

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,8 @@
 const asyncHandler = require('express-async-handler');
 const Goal = require('../models/goalModel');
+
 const getGoals = asyncHandler(async (req,res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
     res.status(200).json(goals)
 })
 
@@ -12,7 +13,8 @@ const setGoals = asyncHandler(async (req,res) => {
         throw new Error('Please add a text field')
     }
     const goal = await Goal.create({
-        text: req.body.text 
+        text: req.body.text,
+        user: req.body.id
     })
     res.status(200).json(goal)
 })
